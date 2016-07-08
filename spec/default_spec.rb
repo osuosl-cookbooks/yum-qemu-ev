bdfeedefe1c78fa257a6680839c3f3d870a3c900
Lance Albertson <lance@osuosl.org>
2016-07-08 10:57:57 -0700

    Initial cookbook for yum-qemu-ev

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -7,4 +7,39 @@ describe 'yum-qemu-ev::default' do
   it 'converges successfully' do
     expect { chef_run }.to_not raise_error
   end
+  it 'installs centos-release-virt-common' do
+    expect(chef_run).to install_package('centos-release-virt-common')
+  end
+  it 'creates qemu-ev yum repository' do
+    expect(chef_run).to create_yum_repository('qemu-ev')
+      .with(
+        description: 'QEMU EV',
+        enabled: true,
+        gpgcheck: true,
+        gpgkey: 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-Virtualization',
+        baseurl: 'http://centos.osuosl.org/$releasever/virt/$basearch/kvm-common/'
+      )
+  end
+  %w(ppc64 ppc64le).each do |a|
+    context "setting arch to #{a}" do
+      cached(:chef_run) do
+        ChefSpec::SoloRunner.new(CENTOS_7_OPTS) do |node|
+          node.automatic['kernel']['machine'] = a
+        end.converge(described_recipe)
+      end
+      it 'not install centos-release-virt-common' do
+        expect(chef_run).to_not install_package('centos-release-virt-common')
+      end
+      it 'creates qemu-ev yum repository' do
+        expect(chef_run).to create_yum_repository('qemu-ev')
+          .with(
+            description: 'QEMU EV',
+            enabled: true,
+            gpgcheck: true,
+            gpgkey: 'http://ftp.osuosl.org/pub/osl/repos/yum/RPM-GPG-KEY-osuosl',
+            baseurl: 'http://ftp.osuosl.org/pub/osl/repos/yum/openpower/centos-$releasever/$basearch/RHEV'
+          )
+      end
+    end
+  end
 end
--- test/integration/default/serverspec/server_spec.rb
+++ test/integration/default/serverspec/server_spec.rb
@@ -1,3 +1,24 @@
 require 'serverspec'
 
 set :backend, :exec
+
+describe yumrepo('qemu-ev') do
+  it { should exist }
+  it { should be_enabled }
+end
+
+describe command('yum install -y qemu-kvm') do
+  its(:exit_status) { should eq 0 }
+end
+
+case os[:arch]
+when 'x86_64'
+  qemu_pkg = 'qemu-kvm-ev-2'
+when 'ppc64', 'ppc64le'
+  qemu_pkg = 'qemu-kvm-rhev-2'
+end
+
+describe command('/usr/libexec/qemu-kvm --version') do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match(/#{qemu_pkg}/) }
+end
