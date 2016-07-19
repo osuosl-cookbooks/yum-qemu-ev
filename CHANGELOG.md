39ceb08dcdd75df62ba46e2dd3c5729662d77ec5
Manatee <github@osuosl.org>
2016-07-19 18:25:53 +0000

    Automatic major-level version bump to v1.0.0 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ yum-qemu-ev CHANGELOG
 This file is used to list changes made in each version of the
 yum-qemu-ev cookbook.
 
+1.0.0 (2016-07-19)
+------------------
+- Initial cookbook
+
 0.1.0
 -----
 - Initial release of yum-qemu-ev
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ issues_url       'https://github.com/osuosl-cookbooks/yum-qemu-ev/issues'
 source_url       'https://github.com/osuosl-cookbooks/yum-qemu-ev'
 description      'Installs/Configures yum-qemu-ev'
 long_description 'Installs/Configures yum-qemu-ev'
-version          '0.1.0'
+version          '1.0.0'
 
 depends          'yum'
 
