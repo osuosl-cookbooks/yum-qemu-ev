ac1967ae74f799e983fc9f9c7c084c777d1330e7
Lance Albertson <ramereth@gmail.com>
2018-12-10 12:00:02 -0800

    Revert "Switch ppc64le to using upstream builds"

--- test/integration/default/serverspec/server_spec.rb
+++ test/integration/default/serverspec/server_spec.rb
@@ -11,14 +11,28 @@ describe command('yum install -y qemu-kvm') do
   its(:exit_status) { should eq 0 }
 end
 
+case os[:arch]
+when 'x86_64'
+  qemu_pkg = 'qemu-kvm-ev-2'
+when 'ppc64', 'ppc64le'
+  qemu_pkg = 'qemu-kvm-rhev-2'
+end
+
 describe command('/usr/libexec/qemu-kvm --version') do
   its(:exit_status) { should eq 0 }
-  its(:stdout) { should match(/qemu-kvm-ev-2/) }
+  its(:stdout) { should match(/#{qemu_pkg}/) }
 end
 
 # Ensure this package was installed using the correct key
-describe command('rpm -qi qemu-kvm-ev | grep Signature') do
-  its(:stdout) { should match(/Key ID 7aebbe8261e8806c/) }
+case os[:arch]
+when 'x86_64'
+  describe command('rpm -qi qemu-kvm-ev | grep Signature') do
+    its(:stdout) { should_not match(/Key ID 2df30655a70b13b7/) }
+  end
+when 'ppc64', 'ppc64le'
+  describe command('rpm -qi qemu-kvm-ev | grep Signature') do
+    its(:stdout) { should match(/Key ID 2df30655a70b13b7/) }
+  end
 end
 
 %w(base extras updates).each do |r|
