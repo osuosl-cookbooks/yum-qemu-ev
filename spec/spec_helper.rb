afc37fb7ccdae1015b299a6a80055ad6c5f631f0
David Gasper <dmgasper@green.workstation.cass.oregonstate.edu>
2019-04-11 11:16:09 -0700

    Update ChefSpec platform versions. Add Debian 9

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,18 +5,24 @@ ChefSpec::Coverage.start! { add_filter 'yum-qemu-ev' }
 
 CENTOS_7_OPTS = {
   platform: 'centos',
-  version: '7.2.1511',
+  version: '7.4.1708',
   log_level: :fatal,
 }.freeze
 
 CENTOS_6_OPTS = {
   platform: 'centos',
-  version: '6.8',
+  version: '6.9',
   log_level: :fatal,
 }.freeze
 
 DEBIAN_8_OPTS = {
   platform: 'debian',
   version: '8.4',
+  log_level: :fatal,
+}.freeze
+
+DEBIAN_9_OPTS = {
+  platform: 'debian',
+  version: '9.3',
   log_level: :fatal,
 }.freeze
