adeff04cf6546d34056b4ddf1fcc218caa1c176e
David Gasper <dmgasper@ultraviolet.workstation.osuosl.bak>
2019-04-12 14:10:28 -0700

    Removed CENTOS_6, DEBIAN_8, DEBIAN_9

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,21 +8,3 @@ CENTOS_7_OPTS = {
   version: '7.4.1708',
   log_level: :fatal,
 }.freeze
-
-CENTOS_6_OPTS = {
-  platform: 'centos',
-  version: '6.9',
-  log_level: :fatal,
-}.freeze
-
-DEBIAN_8_OPTS = {
-  platform: 'debian',
-  version: '8.4',
-  log_level: :fatal,
-}.freeze
-
-DEBIAN_9_OPTS = {
-  platform: 'debian',
-  version: '9.3',
-  log_level: :fatal,
-}.freeze
