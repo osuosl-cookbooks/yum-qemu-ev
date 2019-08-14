7442e62ba7e625e87b0ab3398911e20c44078dfb
David Gasper <dmgasper@osuosl.org>
2019-08-14 17:59:40 +0000

    Chef 14 compatability fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,11 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
-ChefSpec::Coverage.start! { add_filter 'yum-qemu-ev' }
-
 CENTOS_7_OPTS = {
   platform: 'centos',
-  version: '7.4.1708',
-  log_level: :fatal,
+  version: '7',
 }.freeze
+
+RSpec.configure do |config|
+  config.log_level = :warn
+end
