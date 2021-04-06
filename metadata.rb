ac58c2af74285a1b4c21a7f52abc3856ca274f87
Ryan <kennedy@osuosl.org>
2021-04-06 09:15:40 -0700

    Update Chef dependency to >= 16

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chef@osuosl.org'
 source_url       'https://github.com/osuosl-cookbooks/yum-qemu-ev'
 issues_url       'https://github.com/osuosl-cookbooks/yum-qemu-ev/issues'
 license          'Apache-2.0'
-chef_version     '>= 14.0'
+chef_version     '>= 16.0'
 description      'Installs/Configures yum-qemu-ev'
 version          '2.5.1'
 
